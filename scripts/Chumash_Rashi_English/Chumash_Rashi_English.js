57ee7ceb1eb2661ef0bef709b77ba01a9c0afd2c
yparitcher <y@paritcher.com>
2020-04-27 14:32:38 -0500

    alignment

--- scripts/Chumash_Rashi_English/Chumash_Rashi_English.js
+++ scripts/Chumash_Rashi_English/Chumash_Rashi_English.js
@@ -14,23 +14,23 @@ for (let x=0; x<jsonData.he.length; x++) {
 					count = (parseInt(count) + 1).toString();
 					fs.writeFileSync(process.argv[6], count);
 					file = directory + count.padStart(2, "0") + "_" + jsonData.alts[x][i]["en"][0].replace(/ /g, "_") + ".html";
-					fs.writeFileSync(file, "<title>" + jsonData.alts[x][i]["he"][0] + "</title>\n");
-					fs.appendFileSync(file, "<h2>" + jsonData.alts[x][i]["he"][0] + "</h2>\n");
-					fs.appendFileSync(file, "<h3>" + "פרק " + gematria(x+1) + "</h3>\n");
+					fs.writeFileSync(file, '<title class="title">' + jsonData.alts[x][i]["he"][0] + "</title>\n");
+					fs.appendFileSync(file, '<h2 class="h2">' + jsonData.alts[x][i]["he"][0] + "</h2>\n");
+					fs.appendFileSync(file, '<h3 class="h3">' + "פרק " + gematria(x+1) + "</h3>\n");
 				} else if (jsonData.alts[x] != undefined && jsonData.alts[x][i] != undefined){
-					fs.appendFileSync(file, "<h2>" + jsonData.alts[x][i]["he"][0] + "</h2>\n");
+					fs.appendFileSync(file, '<h2 class="h2">' + jsonData.alts[x][i]["he"][0] + "</h2>\n");
 				} else if (i == 0){
-					fs.appendFileSync(file, "<h3>" + "פרק " + gematria(x+1) + "</h3>\n");
+					fs.appendFileSync(file, '<h3 class="h3">' + "פרק " + gematria(x+1) + "</h3>\n");
 				}
-				fs.appendFileSync(file, '<p class="chumash"><b>' + gematria(i+1) + " </b>" + jsonData.he[x][i] + "</p>\n");
+				fs.appendFileSync(file, '<p class="right"><b>' + gematria(i+1) + " </b>" + jsonData.he[x][i] + "</p>\n");
 				if (jsonData.text[x] != "" && jsonData.text[x][i] != undefined) {
-					fs.appendFileSync(file, '<p class="chumashE">' + jsonData.text[x][i] + "</p>\n");
+					fs.appendFileSync(file, '<p class="left">' + jsonData.text[x][i] + "</p>\n");
 				}
 				if (commentaryData.he[x]!= undefined && commentaryData.he[x][i] != 0 && commentaryData.he[x][i] != undefined ) {
 					for (let y=0; y<commentaryData.he[x][i].length ; y++) {
-						fs.appendFileSync(file, '<p class="rashi">' + commentaryData.he[x][i][y] + "</p>\n");
+						fs.appendFileSync(file, '<p class="right">' + commentaryData.he[x][i][y] + "</p>\n");
 						if (commentaryData.text[x] != undefined && commentaryData.text[x][i] != undefined && commentaryData.text[x][i][y] != undefined) {
-							fs.appendFileSync(file, '<p class="rashiE">' + commentaryData.text[x][i][y] + "</p>\n");
+							fs.appendFileSync(file, '<p class="left">' + commentaryData.text[x][i][y] + "</p>\n");
 						}
 					}
 				}
